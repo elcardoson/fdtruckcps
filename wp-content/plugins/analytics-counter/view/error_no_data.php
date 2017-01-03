40a573cb2de0eed9c335a6cd21dcc2987c93c6bc
Tiago Cardoso <tiago.cardoso@movile.com>
2017-01-03 19:27:08 -0200

    pull backup current site

---
+++ wp-content/plugins/analytics-counter/view/error_no_data.php
@@ -0,0 +1,4 @@
+<div style="display: none;" id="modal_error_no_data">
+    <p style="color:red; font-weight: bold;"><?php _e('Google Analytics Service has reported that the analytics data is still not available.<br>Once Google Analytics account  is connected, usually it can take up to 24 hours to show the stats data.', 'analytics-counter');?></p>
+</div><!-- /.modal -->
+<a class="btn btn-info thickbox" href="#TB_inline?width=400&height=140&inlineId=modal_error_no_data" style="display: none;" id="btn_modal_error_no_data">error</a>
